193fee3687db784c4b2057f4599aa51df61fbbf3
Harrison White <harrisonkwhite@gmail.com>
2026-01-30 11:39:05 +1000

    Start on inventory code.

---
+++ src/inventory.cpp
@@ -0,0 +1,13 @@
+#include "inventory.h"
+
+struct t_inventory_slot {
+};
+
+struct t_inventory {
+};
+
+t_inventory *InventoryCreate(const zcl::t_i32 slot_cnt, zcl::t_arena *const arena) {
+}
+
+void InventoryAdd(const t_item_type_id item_type_id, const zcl::t_i32 quantity) {
+}
--- src/inventory.h
+++ src/inventory.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "items.h"
+
+struct t_inventory;
+
+t_inventory *InventoryCreate(const zcl::t_i32 slot_cnt, zcl::t_arena *const arena);
+void InventoryAdd(const t_item_type_id item_type_id, const zcl::t_i32 quantity);
