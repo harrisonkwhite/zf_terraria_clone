967822e1e35f7f45a7f6bf41cd82130ecfcf30dc
Harrison White <harrisonkwhite@gmail.com>
2026-02-11 09:20:15 +1000

    Move NPC and inventory code into their own non-world modules.

---
+++ src/inventories.cpp
@@ -0,0 +1,75 @@
+#include "inventories.h"
+
+struct t_inventory {
+    zcl::t_i32 width;
+    zcl::t_array_mut<t_inventory_slot> slots;
+};
+
+t_inventory *InventoryCreate(const zcl::t_v2_i size, zcl::t_arena *const arena) {
+    ZCL_ASSERT(size.x > 0 && size.y > 0);
+
+    const auto result = zcl::ArenaPush<t_inventory>(arena);
+    result->slots = zcl::ArenaPushArray<t_inventory_slot>(arena, size.x * size.y);
+    result->width = size.x;
+
+    return result;
+}
+
+zcl::t_i32 InventoryAdd(t_inventory *const inventory, const t_item_type_id item_type_id, zcl::t_i32 quantity) {
+    ZCL_ASSERT(item_type_id >= 0 && item_type_id < ekm_item_type_id_cnt);
+    ZCL_ASSERT(quantity >= 0);
+
+    if (quantity == 0) {
+        return 0;
+    }
+
+    for (zcl::t_i32 i = 0; i < inventory->slots.len; i++) {
+        const auto slot = &inventory->slots[i];
+
+        quantity = InventoryAddAt(inventory, {i % inventory->width, i / inventory->width}, item_type_id, quantity);
+
+        if (quantity == 0) {
+            return 0;
+        }
+    }
+
+    return quantity;
+}
+
+zcl::t_i32 InventoryAddAt(t_inventory *const inventory, const zcl::t_v2_i slot_pos, const t_item_type_id item_type_id, const zcl::t_i32 quantity) {
+    ZCL_ASSERT(item_type_id >= 0 && item_type_id < ekm_item_type_id_cnt);
+    ZCL_ASSERT(quantity >= 0);
+
+    const auto slot = &inventory->slots[(slot_pos.y * inventory->width) + slot_pos.x];
+
+    if (slot->quantity > 0 && slot->item_type_id != item_type_id) {
+        return quantity;
+    }
+
+    const zcl::t_i32 quantity_to_add = zcl::CalcMin(quantity, g_item_types[item_type_id].quantity_limit - slot->quantity);
+
+    slot->item_type_id = item_type_id;
+    slot->quantity += quantity_to_add;
+
+    return quantity - quantity_to_add;
+}
+
+zcl::t_i32 InventoryRemoveAt(t_inventory *const inventory, const zcl::t_v2_i slot_pos, const zcl::t_i32 quantity) {
+    ZCL_ASSERT(quantity >= 0);
+
+    const auto slot = &inventory->slots[(slot_pos.y * inventory->width) + slot_pos.x];
+
+    const zcl::t_i32 quantity_to_remove = zcl::CalcMin(quantity, slot->quantity);
+
+    slot->quantity -= quantity_to_remove;
+
+    return quantity - quantity_to_remove;
+}
+
+t_inventory_slot InventoryGet(const t_inventory *const inventory, const zcl::t_v2_i slot_pos) {
+    return inventory->slots[(slot_pos.y * inventory->width) + slot_pos.x];
+}
+
+zcl::t_v2_i InventoryGetSize(const t_inventory *const inventory) {
+    return {inventory->width, inventory->slots.len / inventory->width};
+}
--- src/old/world_private.h
+++ src/old/world_private.h
@@ -24,18 +24,6 @@ namespace world {
 
     constexpr zcl::t_v2_i k_tilemap_size = {8000, 400};
 
-    constexpr zcl::t_i32 k_player_respawn_duration = 120;
-    constexpr zcl::t_i32 k_player_invincible_duration = 30;
-    constexpr zcl::t_f32 k_player_move_spd = 1.5f;
-    constexpr zcl::t_f32 k_player_move_spd_acc = 0.2f;
-    constexpr zcl::t_f32 k_player_jump_height = 3.5f;
-    constexpr zcl::t_v2 k_player_origin = zcl::k_origin_center;
-    constexpr zcl::t_i32 k_player_flash_duration = 10;
-
-    constexpr zcl::t_i32 k_npc_limit = 1024;
-    constexpr zcl::t_v2 k_npc_origin = zcl::k_origin_center;
-    constexpr zcl::t_i32 k_npc_flash_duration = 10;
-
     constexpr zcl::t_i32 k_item_drop_limit = 1024;
     constexpr zcl::t_f32 k_item_drop_item_type_icon_scale = 0.5f;
     constexpr zcl::t_v2 k_item_drop_origin = {0.5f, 0.5f};
@@ -52,58 +40,6 @@ namespace world {
     constexpr zcl::t_f32 k_ui_player_inventory_slot_size = 48.0f;
     constexpr zcl::t_f32 k_ui_player_inventory_slot_distance = 64.0f;
     constexpr zcl::t_f32 k_ui_player_inventory_slot_bg_alpha = 0.4f;
-
-    struct t_player_meta {
-        zcl::t_i32 respawn_time;
-
-        zcl::t_i32 health_limit;
-
-        t_inventory *inventory;
-        zcl::t_i32 inventory_hotbar_slot_selected_index;
-    };
-
-    struct t_player_entity {
-        zcl::t_b8 active;
-
-        zcl::t_i32 health;
-        zcl::t_i32 invincible_time;
-
-        zcl::t_i32 item_use_time;
-
-        zcl::t_v2 pos;
-        zcl::t_v2 vel;
-        zcl::t_b8 jumping;
-
-        zcl::t_i32 flash_time; // @note: Could be derived from invincible_time?
-    };
-
-    struct t_npc {
-        zcl::t_i32 health;
-
-        zcl::t_v2 pos;
-
-        t_npc_type_id type_id;
-
-        union {
-            struct {
-                zcl::t_v2 vel;
-            } slime;
-        } type_data;
-
-        zcl::t_i32 flash_time;
-    };
-
-    // @todo: This should be decoupled. It's important to make sure you can't deactivate NPCs during tick for example.
-    struct t_npc_manager {
-        zcl::t_static_array<t_npc, k_npc_limit> buf;
-        zcl::t_static_bitset<k_npc_limit> activity;
-        zcl::t_static_array<zcl::t_i32, k_npc_limit> versions;
-    };
-
-    struct t_npc_id {
-        zcl::t_i32 index;
-        zcl::t_i32 version;
-    };
 
     struct t_item_drop {
         zcl::t_v2 pos;
@@ -256,25 +192,6 @@ namespace world {
     void HurtPlayer(t_player_entity *const player_entity, const zcl::t_i32 damage, t_pop_up_manager *const pop_up_manager, zcl::t_rng *const rng);
 
     void RenderPlayer(const t_player_entity *const player_entity, const zgl::t_rendering_context rc, const t_assets *const assets);
-
-    // ==================================================
-
-    // ============================================================
-    // @section: NPCs
-
-    t_npc_id SpawnNPC(t_npc_manager *const manager, const zcl::t_v2 pos, const t_npc_type_id type_id);
-
-    void HurtNPC(t_npc_manager *const manager, const t_npc_id id, const zcl::t_i32 damage);
-
-    zcl::t_b8 CheckNPCExists(const t_npc_manager *const manager, const t_npc_id id);
-
-    zcl::t_rect_f GetNPCCollider(const zcl::t_v2 pos, const t_npc_type_id type_id);
-
-    void ProcessNPCAIs(t_npc_manager *const npcs, const t_tilemap *const tilemap);
-
-    void ProcessNPCDeaths(t_npc_manager *const npcs);
-
-    void RenderNPCs(const t_npc_manager *const manager, const zgl::t_rendering_context rc, const t_assets *const assets);
 
     // ==================================================
 
