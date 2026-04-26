2cfcdc0c509ca6f2fd8487f12b3ef625b8e68ef8
Harrison White <harrisonkwhite@gmail.com>
2026-04-26 11:13:02 +1000

    Temporarily revert to just dirt and stone world layers, improve the code for this.

--- src/world_gen.cpp
+++ src/world_gen.cpp
@@ -5,7 +5,7 @@
 t_tilemap_core *WorldGen(const zcl::t_v2_i size, zcl::t_rng *const rng, zcl::t_arena *const arena, zcl::t_arena *const temp_arena) {
     const auto tilemap = TilemapCoreCreate(size, arena);
 
-    const auto calc_lvls = [size, rng, temp_arena](const zcl::t_i32 base, const zcl::t_i32 variation_range, const zcl::t_f32 variation_chance) {
+    const auto calc_lvl_offsets = [size, rng, temp_arena](const zcl::t_i32 variation_range, const zcl::t_f32 variation_chance) {
         ZCL_ASSERT(variation_chance >= 0.0f && variation_chance <= 1.0f);
 
         const auto result = zcl::ArenaPushArray<zcl::t_i32>(temp_arena, size.x);
@@ -14,7 +14,7 @@ t_tilemap_core *WorldGen(const zcl::t_v2_i size, zcl::t_rng *const rng, zcl::t_a
             zcl::t_i32 offs_pen = zcl::RandGenI32InRange(rng, 0, variation_range);
 
             for (zcl::t_i32 x = 0; x < size.x; x++) {
-                result[x] = base + offs_pen;
+                result[x] = offs_pen;
 
                 if (zcl::RandGenPerc(rng) < variation_chance) {
                     const zcl::t_b8 down = (offs_pen == 0 || zcl::RandGenPerc(rng) < 0.5f) && offs_pen < variation_range - 1;
@@ -26,22 +26,24 @@ t_tilemap_core *WorldGen(const zcl::t_v2_i size, zcl::t_rng *const rng, zcl::t_a
         return result;
     };
 
-    const auto dirt_lvls = calc_lvls(size.y / 3, 10, 0.3f);
-    const auto stone_lvls = calc_lvls((size.y / 3) + 20, 5, 0.5f);
+    const zcl::t_i32 dirt_lvl_base = size.y / 3;
+    const auto dirt_lvl_offsets = calc_lvl_offsets(10, 0.3f);
 
-    for (zcl::t_i32 y = 0; y < size.y; y++) {
+    const zcl::t_i32 stone_lvl_base_rel_to_dirt_lvl = 15;
+    const auto stone_lvl_offsets = calc_lvl_offsets(5, 0.5f);
+
+    for (zcl::t_i32 y = dirt_lvl_base; y < size.y; y++) {
         for (zcl::t_i32 x = 0; x < size.x; x++) {
-            if (y >= stone_lvls[x]) {
+            const zcl::t_i32 dirt_lvl = dirt_lvl_base + dirt_lvl_offsets[x];
+            const zcl::t_i32 stone_lvl = dirt_lvl_base + stone_lvl_base_rel_to_dirt_lvl + stone_lvl_offsets[x];
+
+            if (y >= stone_lvl) {
                 TilemapCoreAdd(tilemap, {x, y}, ek_tile_type_id_stone);
-            } else if (y > dirt_lvls[x] + 2) {
+            } else if (y >= dirt_lvl) {
                 TilemapCoreAdd(tilemap, {x, y}, ek_tile_type_id_dirt);
-            } else if (y >= dirt_lvls[x]) {
-                TilemapCoreAdd(tilemap, {x, y}, ek_tile_type_id_grass);
             }
         }
     }
-
-    // static_assert(false, "dodgy above!");
 
     return tilemap;
 }
