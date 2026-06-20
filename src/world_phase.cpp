504818d30bcbff47f201fe7b812bc9f9057b2c08
Harrison White <harrisonkwhite@gmail.com>
2026-06-20 20:35:43 +1000

    Prevent toggling inventory view while player is dead, have it close.

--- src/world_phase.cpp
+++ src/world_phase.cpp
@@ -224,9 +224,9 @@ t_world_phase_tick_result_id WorldPhaseTick(t_world_phase *const world, const t_
 
     // ------------------------------
 
-    ProcessPlayerInventoryUIInteraction(world, input_state);
-
     if (PlayerCheckAlive(world->player_entity)) {
+        ProcessPlayerInventoryUIInteraction(world, input_state);
+
         PlayerUpdateTimers(world->player_entity);
 
         PlayerProcessInventoryHotbarUpdates(world->player_meta, input_state);
@@ -234,6 +234,8 @@ t_world_phase_tick_result_id WorldPhaseTick(t_world_phase *const world, const t_
         PlayerUpdateMovement(world->player_entity, input_state, k_gravity, world->tilemap);
 
         PlayerProcessItemUsage(world->player_entity, input_state, world->player_meta, world->item_drop_manager, world->camera, world->tilemap, world->hitbox_manager, screen_size, temp_arena);
+    } else {
+        world->ui.player_inventory_open = false;
     }
 
     NPCsProcessAIs(world->npc_manager, k_gravity, world->player_entity, world->tilemap, world->rng);
